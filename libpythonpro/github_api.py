19f77af48a58fb93dbf4ba1b8ac2b670cc962809
adiocwball <adiowsy@gmail.com>
2020-04-28 12:27:35 -0300

    configurando Travis CI com python 3.7

--- libpythonpro/github_api.py
+++ libpythonpro/github_api.py
@@ -3,10 +3,11 @@ import requests
 
 def buscar_avatar(usuario):
     """
-    Busca o avatar de um usuário no Github
+    Busca o avatar de um usuário no Github :
 
-    :param usuario: str com o nome de usuário no github
-    :return: str com o link do avatar
+    param usuario: str com o nome de usuário no github  :
+
+    return: str com o link do avatar
     """
     url = f'https://api.github.com/users/{usuario}'
     resp = requests.get(url)
